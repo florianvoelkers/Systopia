6465e4d45c56083ef6b2dcbe28b32a8f5924b8bc
Florian Voelkers <florian.voelkers@mail.de>
2018-03-22 19:42:45 +0100

    SelectAllOfTag - Suche nach einem bestimmten Tag von GameObjects und selektiere sie im Editor

---
+++ Systopia/Assets/Scripts/Editor/SelectAllOfTag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard {
+
+	public string searchTag =  "Your tag here";
+
+	[MenuItem ("My Tools/Select all of tag...")]
+	static void SelectAllOfTagWizard () {
+		ScriptableWizard.DisplayWizard<SelectAllOfTag> ("Select all of tag...", "Make selection");
+	}
+
+	void OnWizardCreate () {
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag (searchTag);
+		Selection.objects = gameObjects;
+	}
+}
