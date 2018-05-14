aa6291b0f729d8f68c61c6292b0b0c734ad73eb9
Florian Voelkers <florian.voelkers@mail.de>
2018-05-14 23:28:52 +0200

    Löschen von Items und Anzeige des Sprites

--- Systopia/Assets/Scripts/Editor/Inventory/ItemEditor.cs
+++ Systopia/Assets/Scripts/Editor/Inventory/ItemEditor.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class ItemEditor : EditorWindow {
 
 	//private Color darkModeBlack = new Color (0.75f, 0.75f, 0.75f);
-	private Color darkModeOrange = new Color (0.9921f, 0.4588f, 0.1294f);
+	//private Color darkModeOrange = new Color (0.9921f, 0.4588f, 0.1294f);
 	private GUIStyle labelStyle = new GUIStyle ();
 	private string[] itemTypes = { "All Items", "Consumables", "Weapons", "Other" };
 	private int selectedItemType = 0;
@@ -49,14 +49,24 @@ public class ItemEditor : EditorWindow {
 			for (int i = 0; i < itemList.Length; i++) {
 				GUILayout.BeginHorizontal ();
 				collapse[i] = EditorGUILayout.Foldout (collapse[i], itemList[i].itemName);
-				if (GUILayout.Button ("Delete Item")) {
-					Debug.Log ("delete item");
+				if (GUILayout.Button ("Delete Item", GUILayout.MaxWidth (100f))) {
+					FileUtil.DeleteFileOrDirectory (Application.dataPath + "/Resources/Items/" + itemList[i].name + ".asset");
+					#if UNITY_EDITOR
+					UnityEditor.AssetDatabase.Refresh ();
+					#endif
 				}
 				GUILayout.EndHorizontal ();
 				if (collapse[i]) {
 					GUILayout.BeginHorizontal ();
 					GUILayout.Space (30f);
-					itemList[i].itemName = EditorGUILayout.TextField ("Item Name", itemList[i].itemName);
+					itemList[i].itemName = EditorGUILayout.TextField ("Item Name", itemList[i].itemName, GUILayout.MaxWidth(450f));
+					itemList [i].itemValue = EditorGUILayout.IntField ("Item Value", itemList [i].itemValue, GUILayout.MaxWidth(450f));
+					GUILayout.EndHorizontal ();
+					GUILayout.BeginHorizontal ();
+					GUILayout.Space (30f);
+					itemList [i].itemSprite = (Sprite)EditorGUILayout.ObjectField ("Item Sprite", itemList [i].itemSprite, typeof(Sprite), true, GUILayout.MaxWidth(450f), GUILayout.MaxHeight (64f));
+					EditorGUILayout.PrefixLabel ("Item Description");
+					itemList [i].itemDescription = EditorGUILayout.TextArea (itemList [i].itemDescription, GUILayout.MaxWidth(300f), GUILayout.Height (64f));
 					GUILayout.EndHorizontal ();
 				}
 
@@ -69,6 +79,7 @@ public class ItemEditor : EditorWindow {
 		} else if (selectedItemType == 3) {
 
 		}
+		Debug.Log (position.width);
 	}
 
 	private void CreateItem () {
