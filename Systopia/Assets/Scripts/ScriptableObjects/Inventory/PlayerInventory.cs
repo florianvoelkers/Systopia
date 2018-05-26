fdec8ad5b1df560bcfc5fa7463e0997c37c90db3
Florian Voelkers <florian.voelkers@mail.de>
2018-05-26 18:37:35 +0200

    SO für das PlayerInventory

---
+++ Systopia/Assets/Scripts/ScriptableObjects/Inventory/PlayerInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu (menuName = "PlayerInventory")]
+public class PlayerInventory : ScriptableObject {
+
+	public List <InventoryItem> items;
+
+	public void AddItem (InventoryItem item) {
+		items.Add (item);
+	}
+
+	public void RemoveItem (InventoryItem item) {
+		items.Remove (item);
+	}
+}
