4e6fa1e1acdd5b011a2eeaaf4dbe1f20e3655290
Florian Voelkers <florian.voelkers@mail.de>
2018-06-05 15:29:49 +0200

    Items werden im Inventory angezeigt und sind auswählbar

---
+++ Systopia/Assets/Scripts/MonoBehaviours/UserInterface/InventoryManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class InventoryManager : MonoBehaviour {
+
+	[SerializeField] private PlayerInventory inventory;
+	[SerializeField] private Image[] groupButtons;
+	[SerializeField] private Sprite tabletGroup;
+	[SerializeField] private Sprite tabletGroupSelected;
+	[SerializeField] private const int maximumItemCount = 18;
+	[SerializeField] private GameObject itemPrefab;
+	[SerializeField] private Transform allItems;
+	[SerializeField] private Sprite itemBorder;
+	[SerializeField] private Sprite itemBorderSelected;
+	[SerializeField] private Sprite itemBorderEmpty;
+
+	private GameObject[] items;
+	private Type[] types = {typeof(Consumable), typeof(Weapon), typeof(Wearable), typeof(QuestItem)};
+	private Type currentType;
+	private int selectedGroup;
+	private List <Item> currentItems;
+	private int selectedItem;
+
+	private void Awake () {	 
+		items = new GameObject[maximumItemCount];
+		selectedGroup = 0;
+		groupButtons [selectedGroup].sprite = tabletGroupSelected;
+		currentType = types [selectedGroup];
+		FindAllItemsOfType ();
+	}
+	
+	public void SelectGroup (int selected) {
+		groupButtons [selectedGroup].sprite = tabletGroup;
+		selectedGroup = selected;
+		groupButtons [selectedGroup].sprite = tabletGroupSelected;
+		currentType = types [selectedGroup];
+		selectedItem = -1;
+		FindAllItemsOfType ();
+	}
+
+	private void FindAllItemsOfType () {
+		currentItems = new List<Item> ();
+		for (int i = 0; i < inventory.items.Count; i++) {
+			if (inventory.items [i].GetType () == currentType) {
+				currentItems.Add (inventory.items [i]);
+			}
+		}
+		DisplayAllItems ();
+	}
+
+	private void DisplayAllItems () {
+		if (items[0] == null) {
+			for (int i = 0; i < items.Length; i++) {
+				items [i] = Instantiate (itemPrefab, allItems);
+				int localIndex = i;
+				UnityEngine.Events.UnityAction itemSelection = () => {
+					this.SelectItem (localIndex);
+				};
+				items [i].GetComponent<Button> ().onClick.AddListener (itemSelection);
+			}
+		}
+
+		for (int i = 0; i < items.Length; i++) {
+			if (i < currentItems.Count) { 
+				items [i].transform.GetComponent<Image> ().sprite = itemBorder;
+				items [i].transform.GetComponentInChildren <Text> ().enabled = true;
+				items [i].transform.GetComponentInChildren <Text> ().text = currentItems [i].itemName;
+				items [i].transform.GetChild (1).GetComponent <Image> ().enabled = true;
+				items [i].transform.GetChild (1).GetComponent <Image> ().sprite = currentItems [i].itemSprite;
+			} else {
+				items [i].transform.GetComponent<Image> ().sprite = itemBorderEmpty;
+				items [i].transform.GetComponentInChildren <Text> ().enabled = false;
+				items [i].transform.GetChild (1).GetComponent <Image> ().enabled = false;
+			}
+		}
+	}
+
+	public void SelectItem (int index) {
+		if (index < currentItems.Count) {
+			if (selectedItem != -1) {
+				items [selectedItem].transform.GetComponent<Image> ().sprite = itemBorder;
+			}
+			selectedItem = index;
+			items [index].transform.GetComponent<Image> ().sprite = itemBorderSelected;
+		}
+	}
+
+}
