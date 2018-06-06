08cab6171fba436e69e77fe0b9e4a26f31955f00
Florian Voelkers <florian.voelkers@mail.de>
2018-06-06 15:00:15 +0200

    Stats für Items hinzugefügt und einen StatsEditor im Inspector

---
+++ Systopia/Assets/Scripts/ScriptableObjects/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu (menuName = "Player/Stats")]
+public class PlayerStats : ScriptableObject {
+
+	public Stat [] stats;
+}
---
+++ Systopia/Assets/Scripts/Stats/StatBonus.cs
@@ -0,0 +1,10 @@
+﻿public class StatBonus {
+
+	public int value { get; set; }
+	public string statType { get; set; }
+
+	public StatBonus (int value, string statType) {
+		this.value = value;
+		this.statType = statType;
+	}
+}
