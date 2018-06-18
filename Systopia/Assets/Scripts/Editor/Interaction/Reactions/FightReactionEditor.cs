024586d89844922222179856857ab1a889c328e3
Florian Voelkers <florian.voelkers@mail.de>
2018-06-18 19:10:59 +0200

    Icons für die Fighting NPCs und FightReaction + FightReactionEditor

---
+++ Systopia/Assets/Scripts/Editor/Interaction/Reactions/FightReactionEditor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+[CustomEditor (typeof (FightReaction))]
+public class FightReactionEditor : ReactionEditor {
+
+	protected override string GetFoldoutLabel () {
+		return "Fight Reaction";
+	}
+}
