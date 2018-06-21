edea6b325f2f7ae4298238c9a22f0e85d885a653
Florian Voelkers <florian.voelkers@mail.de>
2018-06-21 16:07:52 +0200

    sceneController hinzugefügt zum Anfangsszenenskript

--- Systopia/Assets/AnfangsSzenenScript.cs
+++ Systopia/Assets/AnfangsSzenenScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class AnfangsSzenenScript : MonoBehaviour {
 
     private TextManager textmanager;
+	private SceneController sceneController;
 
 	// Use this for initialization
 	void Start () {
         textmanager = FindObjectOfType<TextManager>();
+		sceneController = FindObjectOfType<SceneController> ();
 	}
 	//textmanager.DisplayMessage(message,textColor,delay);
+	//sceneController.StartGame ();
 	// Update is called once per frame
 	void Update () {
 		
