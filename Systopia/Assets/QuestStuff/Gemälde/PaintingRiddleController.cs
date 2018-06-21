ac06adeda3fabe8b419a8a74c10203a2a63a13c3
Florian Voelkers <florian.voelkers@mail.de>
2018-06-21 23:33:25 +0200

    TechnoHQ Interactables fertig + Gemälde und Code Rätsel

---
+++ Systopia/Assets/QuestStuff/Gemälde/PaintingRiddleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaintingRiddleController : MonoBehaviour {
+
+	[SerializeField] private GameObject paintingRiddleCanvas;
+	[SerializeField] private InputField inputField;
+	[SerializeField] private string solution;
+	[SerializeField] private ReactionCollection solutionCorrectReaction;
+
+	public void CheckSolution () {
+		if (inputField.text == solution) {
+			paintingRiddleCanvas.SetActive (false);
+			solutionCorrectReaction.React ();
+		}
+	}
+}
--- Systopia/Assets/Scripts/MonoBehaviours/UserInterface/CodeErrorController.cs
+++ Systopia/Assets/Scripts/MonoBehaviours/UserInterface/CodeErrorController.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodeErrorController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	[SerializeField] private GameObject codeErrorCanvas;
+
+	public void HideCanvas () {
+		codeErrorCanvas.SetActive (false);
 	}
 }
