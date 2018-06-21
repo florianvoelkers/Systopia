225caf3cb2a8ca8aff3629f4a42f8237e43572e9
Nikolai Stecker <Nikolai.Stecker@gmx.de>
2018-06-21 15:38:37 +0200

    AnfangsSzenen animationen eingefügt, text fehlt noch

---
+++ Systopia/Assets/AnfangsSzenenScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnfangsSzenenScript : MonoBehaviour {
+
+    private TextManager textmanager;
+
+	// Use this for initialization
+	void Start () {
+        textmanager = FindObjectOfType<TextManager>();
+	}
+	//textmanager.DisplayMessage(message,textColor,delay);
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
